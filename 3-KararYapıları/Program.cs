b1f749441b3c2898bcdb0e67ba04d26ddb70868c
bahadircankardes <kardes126@gmail.com>
2024-08-08 20:48:10 +0300

    karar yapilari

---
+++ 3-KararYapıları/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _3_KararYapıları;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Klavyeden Girilensuyun sicakligina gore katıi sivi gaz mesajini ekrana basalim
+
+        short SuSicakligi;
+
+        Console.Write("Suyun Sicakligini Giriniz:");
+
+        SuSicakligi = short.Parse(Console.ReadLine());
+
+        if (SuSicakligi == 0)
+        {
+            Console.WriteLine();
+        }
+
+        else
+
+        {
+            Console.WriteLine();
+        }
+
+        if (SuSicakligi > 100)
+        {
+            Console.WriteLine("Gaz Haldedir.");
+        }
+
+        else if (SuSicakligi > 0)
+        {
+            Console.WriteLine("Sivi Haldedir.");
+        }
+
+        else
+
+        {
+            Console.WriteLine("Kati Haldedir.");
+        }
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
+
--- DegiskenTanımlama/Program.cs
+++ DegiskenTanımlama/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, Qwerty!");
+        Console.WriteLine("Hello, Guys!");
         Console.WriteLine("Hello, CanDeniz!");
     }
 }
